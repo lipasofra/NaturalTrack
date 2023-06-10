c1f98440c2fcb17d3f7afcbafb84fae6537e9d40
Carlos D. Escobar-Valbuena <carlosdavidescobar@gmail.com>
2023-06-10 13:51:36 -0500

    feat(backend): add chatbot and query agent

---
+++ backend/disaster_explorer/query_agent.py
@@ -0,0 +1,56 @@
+#%%
+import pandas as pd
+from langchain.agents import create_pandas_dataframe_agent, load_tools
+from langchain import OpenAI
+from dotenv import find_dotenv, load_dotenv
+import os
+import re
+load_dotenv(find_dotenv())
+
+
+def run_query(query, openai_key=None, csv_path='sample_data/disasters_1970_2021.csv'):
+    llm = OpenAI(
+        openai_api_key=openai_key if openai_key is not None else os.getenv("OPENAI_API_KEY"),
+        model_name="gpt-3.5-turbo",
+    )
+    tools = load_tools(
+        ["llm-math", "open-meteo-api", "requests_all", "terminal", "python_repl"],
+        llm=llm,
+    )
+
+    df = pd.read_csv(csv_path)
+
+    agent = create_pandas_dataframe_agent(
+        llm=llm,
+        df=df,
+        tools=tools,
+        verbose=True,
+    )
+
+    prompt_template = """
+    You are a helpful assistant that that can answer questions \
+    in an a step by step way, making sure to have the right answer. \
+    Let's work out the following problem: \
+        
+    {prompt}. 
+    Now, as a researcher, you are tasked with investigating the provided response options, \
+    list the flaws and faulty logic, as well as the correct statements of each answer option.\
+    Let's work out step in a step by step way to be sure we have all the errors and correct statements. \
+    Then, after discussing the reseached options, as a resolver, you are tasked with 1) finding which of \
+    the answer the reseacher though of was best, 2) improving that answer, and 3) returning the improved answer in full. \
+    Let's work this out in a step by step way to be sure we have the right answer. \
+    At the end, return a user friendly answer as per the initial question following the result result of step 3. \
+    Display the pyspark/pandas code used to transform and process the data, \
+    and show a sample code to plot the resulting prediction plus the initial dataset.
+    """
+
+    try:
+        response= agent.run(query)
+        
+    except Exception as e:
+             response = str(e)
+             if response.startswith("Could not parse LLM output: `"):
+                  response = response.removeprefix("Could not parse LLM output: `").removesuffix("`")
+    
+    return response
+# %%
